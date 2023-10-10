3b886dd18e53e98f47b13cb09c3a983177134e99
richardmathew2004 <146074830+richardmathew2004@users.noreply.github.com>
2023-10-10 16:28:48 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+/*
+ * file        : hello java
+ * description  : printing hello java
+ * author       : richard
+ * version      :1
+ * date         :26/09/23
+ */
+package javaproject;
+
+public class Helloworld {
+	public static void main(String[]args) {
+	System.out.println("Hello world");
+
+}
+}
